287da7ea4dfde0482c371da379534bc24ffb68db
omsurti <omsurti29001@gmail.com>
2023-01-04 18:19:23 +0530

    change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 
 require('./db/conn')
 const projectdata = require("./model/projectschema")
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.use(express.json());
 
 app.use(require('./router/auth'));
