7513d330c25c670eea4eb89c68448c47b90be2fa
omsurti <omsurti29001@gmail.com>
2023-01-04 20:02:47 +0530

    vercel_config

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
-const apis = require("./api/auth")
-
+const apis = require("./api")
 
 require("./db/conn")
 // const projectdata = require("./model/projectschema")
+// app.get('/about', (req, res) => res.send('About Page Route'));
+
 const PORT = process.env.PORT || 8080
 app.use(express.json())
-app.get('/about', (req, res) => res.send('About Page Route'));
 app.use("/", apis)
 // app.use("/portfolio", apis2)
-
 
 app.listen(PORT, () => {
   console.log("running " + PORT)
