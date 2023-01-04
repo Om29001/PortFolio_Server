4c4e7fff9cd997d43d0f5fba535ae6243a187ef7
omsurti <omsurti29001@gmail.com>
2023-01-04 19:48:17 +0530

    vercel_config

--- db/conn.js
+++ db/conn.js
@@ -4,4 +4,4 @@ dotenv.config({path : './config.env'});
 
 const DB = process.env.DATABASE;
 
-mongoose.connect(DB ).then(() => { console.log("Connected") }).catch(err => { console.log(err,DB) })
+mongoose.connect(DB ).then(() => { console.log("Connected") }).catch(err => { console.log(err) })
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
-const apis = require("./api")
+const apis = require("./api/auth")
+
 
 require("./db/conn")
 // const projectdata = require("./model/projectschema")
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 8080
 app.use(express.json())
 
 app.use("/", apis)
+// app.use("/portfolio", apis2)
+
 
 app.listen(PORT, () => {
   console.log("running " + PORT)
